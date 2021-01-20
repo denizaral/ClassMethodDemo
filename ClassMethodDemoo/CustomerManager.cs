53ccff9305a20f120f4d1de4090145446c7bedc8
denizaral <araldeniz97@gmail.com>
2021-01-20 17:56:33 +0300

    Add project files.

---
+++ ClassMethodDemoo/CustomerManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemoo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Customer was added. " + customer.Id + " " + customer.Name + " " + customer.Surname);
+        }
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Customer was deleted. " + customer.Id + " " + customer.Name + " " + customer.Surname);
+        }
+        public void List(Customer customer)
+        {
+            Console.WriteLine("Customer was listed. " + customer.Id + " " + customer.Name + " " + customer.Surname);
+        }
+    }
+}
